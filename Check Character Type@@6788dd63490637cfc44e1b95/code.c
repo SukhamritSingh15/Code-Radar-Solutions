3f365f9407c1e796f498955e53900ce0387cf1d8
SukhamritSingh15 <sukhamritsingh1512@gmail.com>
2025-02-17 08:25:04 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,7 +8,7 @@ int main(){
     else if(ch>='0' && ch<='9'){
         printf("Digit");
     }
-    else if((ch>='a'&& ch<='z')||(ch>='A' && ch>='Z')){
+    else if((ch>'a'&& ch<'z')||(ch>'A' && ch>'Z')){
         printf("Consonant");
     }
     else{
