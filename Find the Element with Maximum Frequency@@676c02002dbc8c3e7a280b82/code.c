1184a94dfa6499fd844aae2dd5f11da07e6e4733
SukhamritSingh15 <sukhamritsingh1512@gmail.com>
2025-03-29 17:01:53 +0530

    Solution to Find the Element with Maximum Frequency

---
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for(int i = 0;i<=N-1;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i = 0;i<=N-1;i++){
+        int count =0;
+        for(int j = 0;j<=N-1;j++){
+            if(arr[i] ==arr[j]){
+                count++;
+            }
+        }
+        if(count>N/2){
+            printf("%d",arr[i]);
+            return 0;
+        }
+    }
+    printf("-1\n");
+    return 0;
+}
