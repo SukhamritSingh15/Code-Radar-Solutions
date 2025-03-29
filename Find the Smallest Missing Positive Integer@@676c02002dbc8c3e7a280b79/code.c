0b699b6f4680fb559ab621e1d0141478743fa857
SukhamritSingh15 <sukhamritsingh1512@gmail.com>
2025-03-29 13:09:15 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -8,7 +8,7 @@ int main(){
     }
     for(int i = 0;i<=N-1;i++){
         for(int j = i+1;j<=N-1;j++){
-            if(arr[i]==arr[j]){
+            if(arr[i]>arr[j]){
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
